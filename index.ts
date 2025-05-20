1e0d57e377f6ec14ad22c6189235d6cdb222358c
Luis vera <cocicor.luis.vera@gmail.com>
2025-05-20 15:28:38 -0500

    feat: Corregir URL de origen en la configuración de CORS

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ['https://sigabefront-three.vercel.app', 'http://localhost:3000'],
+  origin: ['https://sigabe-frontend.vercel.app', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
